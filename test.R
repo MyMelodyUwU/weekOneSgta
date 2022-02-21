6aa36337f69a2421f1f3a9d3f68b7d17c8c79a10
Melody <64640893+MyMelodyUwU@users.noreply.github.com>
2022-02-22 10:33:26 +1100

    updateTuesday

--- test.R
+++ test.R
@@ -10,39 +10,62 @@ library("patchwork")
 library("janitor")
 library("usethis")
 
-
 dat = read.table("pulse.dat", header=TRUE)
+# Something that reads the data, a header is a logical variable which shows
+# Which working directory its in.
 
 attach(dat)
+# We are making the data set saved, and easy to find.
 
 stem(dat$pulse)
+# Creates a stem and leaf plot, from the sub category pulse, pulse has been "Head"
+# From the data, which means we take a single column from the data set.
 
 hist(dat$pulse, main = "Student Pulse rates (bpm)")
+
+# Creates a histogram of the data, which in this case is Pulse
 
 boxplot(dat$pulse, horizontal = TRUE,
         main = "Student Pulse rates (bpm)")
 
+# Same shit, but a box plot.
+
 summary(dat$pulse)
 
+# Gives General Information of the data set, which we can see from the results the
+# Min, 1st Quartile, Median, Mean, 3rd Quartile and Max is showing
+
 sd(dat$pulse)
+# Standard deviation.
 
 mean(dat$pulse)
+#Mean
 
 dat1 = read.table("encoder.txt", header = TRUE)
 str(dat1) # Look at the data object
 dat1 # Look at all observations
 head(dat1) # display first six observations
 summary(dat1$time) # obtain descriptive statistics
-t.test(dat1$time, mu = 19.25) # Conduct the test
-
-dat1 = read.table("encoder.txt", header = TRUE)
-str(dat1) # Look at the object
-
-summary(dat1$time) # obtain descriptive statistics
-
-t.test(dat1$time, mu = 19.25) # Conduct the test
-
+sd(dat1$time)^2 #Find the SD.
+pValue <- t.test(dat1$time, mu = 19.25)$p.value
+# ifelse(bob < 0.05 <-yes = print("Reject"), no = print("Do not reject"))
+if(pValue < 0.05) {
+  print("Reject")
+} else {
+  print("Do Not Reject")
+}
 t.test(dat1$time, mu = 19.25, alternative = "less")
 
 t.test(dat1$time, mu = 19.25, alternative = "greater")
 
+# Okay, we didn't know why the fuck mu is 19.25, mu is the mean of the POPULATION
+# When we find the descriptive statistics of the data, we realize the mean is
+# 18.22, lol what the fuck right? No, this is coz this is this the
+# mean of the SAMPLE
+
+# When we are constructing our hypothesis, we will need to create a Null Hypothesis
+# and a Alternative Hypothsis.
+
+# In this case, the sample mean is NOT 19.25, but 18.22 from our t test. Therefore,
+# We reject the null hypothesis AND the sample mean was significantly less than the
+# population
